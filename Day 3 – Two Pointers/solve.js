3585d51646e22780b33cb12fdc3a0796f614fd76
ShroukMagdy3 <shorokmagdy15@gmail.com>
2026-02-22 23:23:57 +0200

    add day 3

---
+++ Day 3 – Two Pointers/solve.js
@@ -0,0 +1,44 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+    
+    s = s.toLowerCase();    
+    let cleaned = "";
+    for (let i = 0; i < s.length; i++) {
+        let char = s[i];
+        if ((char >= 'a' && char <= 'z') || (char >= '0' && char <= '9')) {
+            cleaned += char;  
+        }
+    }
+    let reversed = "";
+    for (let i = cleaned.length - 1; i >= 0; i--) {
+        reversed += cleaned[i];
+    }
+    
+    return cleaned === reversed;
+
+};
+
+
+
+
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    if(nums.length === 0 ){
+        return 0;
+    }
+    let j =1;
+    for(let i = 1; i<nums.length ; i++){
+        if(nums[i] !== nums[i-1]){
+            nums[j] =nums[i];
+            j++;
+        }
+    }
+    return j;
+};
