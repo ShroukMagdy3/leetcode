e77ead74ea04c861d5bd92159e253730533d4958
ShroukMagdy3 <shorokmagdy15@gmail.com>
2026-03-05 05:48:26 +0200

    characterReplacement

--- Day 4 – Sliding Window/solve.js
+++ Day 4 – Sliding Window/solve.js
@@ -20,6 +20,26 @@ var lengthOfLongestSubstring = function(s) {
     return mx;
 };
 
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ */
+var characterReplacement = function (s, k) {
+    let mp = new Map();
+    let l = 0, mx = 0, res = 0;
+
+    for (let r = 0; r < s.length; r++) {
+        mp.set(s[r], (mp.get(s[r]) || 0) + 1);
+        mx = Math.max(mx, mp.get(s[r]));
+        while (r - l + 1 - mx > k) {
+            mp.set(s[l], mp.get(s[l]) - 1);
+            l++;
+        }
+        res = Math.max(res, r - l + 1)
+    }
+    return res;
+};
 
 
 
